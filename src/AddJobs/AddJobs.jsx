b24cfb4b875e1b0881dc7e57726c357c65912d3c
unknown <abusiddick84@gmail.com>
2024-05-12 00:12:34 +0600

    Add a job ui done

--- src/AddJobs/AddJobs.jsx
+++ src/AddJobs/AddJobs.jsx
@@ -2,50 +2,44 @@
 const AddJobs = () => {
     return (
         <div>
-            <div className="lg:w-[1000px] m-auto pb-24 mt-16 border-2 rounded-xl">
+            <div className="lg:w-[1000px] m-auto pb-4 mt-16 mb-16 border-2 border-gray-500 rounded-xl">
                 <h1 className="text-3xl mt-10 my-6 text-center">Add A Jobs</h1>
                 <form className="md:w-full mx-auto p-8">
                     <div className="lg:flex lg:gap-6">
                         <div className="lg:w-[450px]">
                             <div className="form-control pb-6">
-                                <input type="text" required name="picture" placeholder="Picture URL" className="input input-bordered" />
+                                <input type="text" required name="picture" placeholder="Picture URL" className="input input-bordered border-gray-500" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="Job Title" placeholder="job_title" className="input input-bordered" />
+                                <input type="text" required name="Job Title" placeholder="job_title" className="input input-bordered border-gray-500" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" name="user_Name" placeholder="User_Name" className="input input-bordered" />
+                                <input type="text" name="user_Name" placeholder="User_Name" className="input input-bordered border-gray-500" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="email" required name="email" placeholder="User_Email" className="input input-bordered" />
+                                <input type="email" required name="email" placeholder="User_Email" className="input input-bordered border-gray-500" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="job_category" placeholder="Job_Category" className="input input-bordered" />
+                                <input type="text" required name="job_category" placeholder="Job_Category" className="input input-bordered border-gray-500" />
                             </div>
                         </div>
                         <div className="lg:w-[450px]">
                             <div className="form-control pb-6">
-                                <input type="text" required name="salary_range" placeholder="Salary_Range" className="input input-bordered" />
+                                <input type="text" required name="salary_range" placeholder="Salary_Range" className="input input-bordered border-gray-500" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="job_description" placeholder="Job_Description" className="input input-bordered" />
+                                <input type="text" required name="job_description" placeholder="Job_Description" className="input input-bordered border-gray-500" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="post_date" placeholder="Job_Post_Date" className="input input-bordered" />
+                                <input type="text" required name="post_date" placeholder="Job_Post_Date" className="input input-bordered border-gray-500" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="application_deadline" placeholder="Application_Deadline" className="input input-bordered" />
+                                <input type="text" required name="application_deadline" placeholder="Application_Deadline" className="input input-bordered border-gray-500" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="applicants_number" placeholder="Job_Applicants_Number" defaultValue={0} className="input input-bordered" />
+                                <input type="text" required name="applicants_number" placeholder="Job_Applicants_Number" defaultValue={0} className="input input-bordered border-gray-500" />
                             </div>
                         </div>
-                    </div>
-                    <div>
-
-                        {/* <div className="form-control pb-6">
-                            <textarea name="description" required placeholder="Short description" className="input input-bordered h-28 w-full rounded-xl p-4" rows="4" cols="50"></textarea>
-                        </div> */}
                     </div>
                     <div className="form-control w-[200px] mt-6 mx-auto">
                         <input type="submit" value="Add" className="btn btn-primary rounded-full text-xl" />
--- src/MyJobs/MyJobs.jsx
+++ src/MyJobs/MyJobs.jsx
@@ -1,8 +1,38 @@
+import { Link } from "react-router-dom";
 
 const MyJobs = () => {
+
     return (
         <div>
-            <h1 className="text-4xl">My jobs</h1>
+            <div className="flex border-[1px]">
+                <div className="w-[45%] max-h-[250px]">
+                    <div className="w-full h-full flex lists-center justify-center">
+                        <img className="w-full h-full" src={image} alt="Avatar Tailwind CSS Component" />
+                    </div>
+                </div>
+                <div className="w-[55%]">
+                    <div className="p-8">
+                        <h1 className="text-2xl font-bold">{item_name}</h1>
+                        <div className="flex gap-16 text-xl my-4">
+                            <p>{stockStatus}</p>
+                            <p>Customization : </p>
+                        </div>
+                        <div className="flex gap-48 pb-4">
+                            <h1 className="text-2xl font-bold">$50000</h1>
+                            <p className="text-2xl">/5</p>
+                        </div>
+                        <div className="flex lg:gap-8">
+                            <Link to={`/update`}>
+                                <button className="btn btn-primary text-xl">Update</button>
+                            </Link>
+                            <button className="btn btn-error text-xl">Delete</button>
+                            <Link to={`/artCraft`}>
+                                <button className="btn btn-success text-xl">Details</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
