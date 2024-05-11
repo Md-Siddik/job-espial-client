12f73ce414d8a3d7abd4a6a87c4c865ad6593429
unknown <abusiddick84@gmail.com>
2024-05-11 23:35:35 +0600

    Working on Add A Job

--- src/AddJobs/AddJobs.jsx
+++ src/AddJobs/AddJobs.jsx
@@ -11,44 +11,44 @@ const AddJobs = () => {
                                 <input type="text" required name="picture" placeholder="Picture URL" className="input input-bordered" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="Job Title" placeholder="item_name" className="input input-bordered" />
+                                <input type="text" required name="Job Title" placeholder="job_title" className="input input-bordered" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" name="subcategory_Name" placeholder="subcategory_Name" className="input input-bordered" />
+                                <input type="text" name="user_Name" placeholder="User_Name" className="input input-bordered" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="stockStatus" placeholder="stockStatus" className="input input-bordered" />
+                                <input type="email" required name="email" placeholder="User_Email" className="input input-bordered" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="price" placeholder="price" className="input input-bordered" />
+                                <input type="text" required name="job_category" placeholder="Job_Category" className="input input-bordered" />
                             </div>
                         </div>
                         <div className="lg:w-[450px]">
                             <div className="form-control pb-6">
-                                <input type="email" required name="email" placeholder="User Email" className="input input-bordered" />
+                                <input type="text" required name="salary_range" placeholder="Salary_Range" className="input input-bordered" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="name" placeholder="User Name" className="input input-bordered" />
+                                <input type="text" required name="job_description" placeholder="Job_Description" className="input input-bordered" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="rating" placeholder="rating" className="input input-bordered" />
+                                <input type="text" required name="post_date" placeholder="Job_Post_Date" className="input input-bordered" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="customization" placeholder="customization" className="input input-bordered" />
+                                <input type="text" required name="application_deadline" placeholder="Application_Deadline" className="input input-bordered" />
                             </div>
                             <div className="form-control pb-6">
-                                <input type="text" required name="processing_time" placeholder="processing_time" className="input input-bordered" />
+                                <input type="text" required name="applicants_number" placeholder="Job_Applicants_Number" defaultValue={0} className="input input-bordered" />
                             </div>
                         </div>
                     </div>
                     <div>
-                        
-                    <div className="form-control pb-6">
-                                <textarea name="description" required placeholder="Short description" className="input input-bordered h-28 w-full rounded-xl p-4" rows="4" cols="50"></textarea>
-                            </div>
+
+                        {/* <div className="form-control pb-6">
+                            <textarea name="description" required placeholder="Short description" className="input input-bordered h-28 w-full rounded-xl p-4" rows="4" cols="50"></textarea>
+                        </div> */}
                     </div>
                     <div className="form-control w-[200px] mt-6 mx-auto">
-                        <input type="submit" value="Add"  className="btn btn-primary rounded-full text-xl"/>
+                        <input type="submit" value="Add" className="btn btn-primary rounded-full text-xl" />
                     </div>
                 </form>
             </div>
